195ad59e622d6476a1882d923ae666cc2d0241b2
nlevites <nlevites@users.noreply.github.com>
2025-09-03 14:32:15 -0700

    Update base URL for GitHub Pages deployment

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/flightplanner/',
+  base: '/172E-climb-performance-calculator/',
   build: {
     outDir: 'dist',
   },
